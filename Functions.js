6b64661bcf6de3fa89c7c88fc79652266f61a4e3
Amit Gupta <amit2606.gupta@gmail.com>
2024-08-04 22:53:26 +0530

    Building Logic

---
+++ Functions.js
@@ -0,0 +1,31 @@
+// Using Function 
+
+// function addNumber(a,b){
+//     return a+b;
+// }
+// let sum = addNumber(5,10);
+// console.log(sum);
+
+
+// function subNumber(a,b){
+//     return  a-b;
+
+// }
+// let sub = subNumber(50,2)
+// console.log(sub);
+
+
+// function multiNumber(a,b){
+//     return (a*b);
+// }
+// let multi = multiNumber(5,10)
+// console.log(multi)
+
+
+// function divNumber(a,b){
+//     return a/b;
+// }
+// let div = divNumber(10,5);
+// console.log(div)
+
+
---
+++ Looping.js
@@ -0,0 +1,28 @@
+//Using For Loop
+console.log("Using for Loop");
+let i = 0;
+for(let i=1; i<=5; i++){
+    console.log(i);
+}
+
+
+// Using whlie Loop
+console.log("Using while Loop");
+let j = 1;
+while (j<=5){
+    console.log(j);
+    j++;
+}
+
+
+
+// Using Do-While Loop
+console.log("Using Do-While Loop");
+let k = 1;
+do{
+    console.log(k);
+    k++;
+}while(k<=5);
+
+
+
