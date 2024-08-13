f668168966d7fab3001a9365652be15d2f3341cf
Amit Gupta <amit2606.gupta@gmail.com>
2024-08-13 12:56:42 +0530

    Implementing Array

--- Array.js
+++ Array.js
@@ -1,6 +1,30 @@
-// let arr = [12,13,14,15,11,23,21] ;
-// let sum = 0;
-// for(let i = 0; i<arr.length; i++){
-//     sum +=arr[i];
-// }
-// console.log(sum)
+//create An Aarray
+// let arr = [1,2,3,4,5]
+// console.log(arr);
+
+
+//Find the Array Length
+// let arr = [1,2,3,4,5]
+// console.log(arr.length);
+
+
+// Access the first aelement of Array
+// let arr = [1,2,3,4,5]
+// console.log(arr[0]);
+
+
+//Access the last Element of Array
+// let arr = [1,2,3,4,6];
+// console.log(arr[arr.length - 1]);
+
+
+//Add element at the and of an Array
+// let arr = [1,2,3,4,5];
+// arr.push(8);
+// console.log(arr);
+
+
+//Remove the last element from an Array
+let arr = [1,2,3,4,5,6,7];
+arr.pop();
+console.log(arr);
